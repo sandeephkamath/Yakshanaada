69780355df6b174e1b60e69f5e389af1033b47fc
Sandeep Kamath <XPlode123>
2019-11-27 20:02:22 +0530

    - Spinner UI fix

--- app/src/main/java/com/lovoctech/yakshanaada/ui/ui/kareoke/KareokeFragment.java
+++ app/src/main/java/com/lovoctech/yakshanaada/ui/ui/kareoke/KareokeFragment.java
@@ -1,5 +1,6 @@
 package com.lovoctech.yakshanaada.ui.ui.kareoke;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,16 +9,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import butterknife.BindView;
 
 import com.lovoctech.yakshanaada.KareokePlayer;
-
-import butterknife.ButterKnife;
-
-import com.lovoctech.yakshanaada.PlayerManager;
 import com.lovoctech.yakshanaada.R;
 import com.lovoctech.yakshanaada.RxBus;
 import com.lovoctech.yakshanaada.YakshaNaadaApplication;
@@ -25,16 +23,56 @@ import com.lovoctech.yakshanaada.model.Shruthi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemSelected;
 
 
 public class KareokeFragment extends Fragment {
 
-    private Spinner taalaSpinner;
-    private KareokePlayer playerManager;
-    private RxBus rxBus;
-
     @BindView(R.id.editText)
     EditText editText;
+
+    @BindView(R.id.buttonNade)
+    Button nadeButton;
+
+    private KareokePlayer playerManager;
+    private int selectedPosition;
+
+    @BindView(R.id.spinner)
+    Spinner taalaSpinner;
+
+    @OnClick(R.id.buttonNade)
+    void nadeClick() {
+        Shruthi shruthi = new Shruthi(
+                R.raw.ds,
+                "DS",
+                "ಕಪ್ಪು 2",
+                "D#",
+                R.mipmap.yakshanaada
+        );
+        playerManager.playNade(shruthi, R.raw.twaritha_trivude_nade, Float.parseFloat(editText.getText().toString()));
+    }
+
+    @OnClick(R.id.buttonBidthige)
+    void bidthigeClick() {
+        Shruthi shruthi = new Shruthi(
+                R.raw.ds,
+                "DS",
+                "ಕಪ್ಪು 2",
+                "D#",
+                R.mipmap.yakshanaada
+        );
+        playerManager.playBidthige(R.raw.twaritha_trivude_bidthige, R.raw.twaritha_trivude_nade, Float.parseFloat(editText.getText().toString()));
+    }
+
+    @OnItemSelected(R.id.spinner)
+    void onSpinnerItemSelected(int index) {
+        selectedPosition = index;
+    }
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -42,69 +80,35 @@ public class KareokeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_kareoke, container, false);
         ButterKnife.bind(this, root);
-        addTaalas(root);
-        rxBus = ((YakshaNaadaApplication) getActivity().getApplication()).bus();
-
-        rxBus.toObservable()
-                .subscribe(object -> {
-                    if (object instanceof Shruthi) {
-                        playShruthi((Shruthi) object);
-                    }
-                });
+        addTaalas();
+        RxBus rxBus = ((YakshaNaadaApplication) Objects.requireNonNull(getActivity()).getApplication()).bus();
 
         playerManager = KareokePlayer.getInstance();
         playerManager.init(getContext(), rxBus);
-
-
-        Button button = (Button) root.findViewById(R.id.buttonNade);
-        button.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //playerManager.release();
-                Shruthi shruthi = new Shruthi(
-                        R.raw.ds,
-                        "DS",
-                        "ಕಪ್ಪು 2",
-                        "D#",
-                        R.mipmap.yakshanaada
-                );
-                playerManager.playNade(shruthi, R.raw.twaritha_trivude_nade, Float.parseFloat(editText.getText().toString()));
-
-            }
-        });
-
-        Button buttonBidthige = (Button) root.findViewById(R.id.buttonBidthige);
-        buttonBidthige.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Shruthi shruthi = new Shruthi(
-                        R.raw.ds,
-                        "DS",
-                        "ಕಪ್ಪು 2",
-                        "D#",
-                        R.mipmap.yakshanaada
-                );
-                playerManager.playBidthige(R.raw.twaritha_trivude_bidthige, R.raw.twaritha_trivude_nade,Float.parseFloat(editText.getText().toString()));
-            }
-        });
-
-
         return root;
     }
 
-    private void addTaalas(View view) {
-        List<String> taals = new ArrayList<String>();
+    private void addTaalas() {
+        List<String> taals = new ArrayList<>();
         taals.add("Twaritha Trivude");
         taals.add("Nidhaana Trivude");
-        taalaSpinner = (Spinner) view.findViewById(R.id.spinner);
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_layout, taals);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(Objects.requireNonNull(getContext()), R.layout.spinner_layout, taals) {
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, null, parent);
+                if (position == selectedPosition) {
+                    view.setBackgroundColor(Color.BLACK);
+                    ((TextView) view).setTextColor(Color.WHITE);
+                } else {
+                    view.setBackgroundColor(Color.WHITE);
+                    ((TextView) view).setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
         taalaSpinner.setAdapter(dataAdapter);
         taalaSpinner.setEnabled(true);
     }
 
-    private void playShruthi(Shruthi shruthi) {
-        //playerManager.release();
-        //playerManager.setShruthi(shruthi);
-    }
 
 }
